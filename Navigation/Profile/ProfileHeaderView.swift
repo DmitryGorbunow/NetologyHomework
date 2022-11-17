05185d132c62db2279eb1584d7a1f7c3548fe0bc
DmitryGorbunow <84672242+DmitryGorbunow@users.noreply.github.com>
2022-11-17 14:14:24 +0500

    add UI

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,70 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Dmitry Gorbunow on 11/15/22.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+    private func getLabel() -> UILabel {
+        let labelFrame = CGRect(x: 160, y: 127, width: 200, height: 20)
+        let label = UILabel(frame: labelFrame)
+        label.text = "Дмитрий Горбунов"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        return label
+    }
+    
+    private func getAvatar() -> UIImageView {
+        let imageFrame = CGRect(x: 16, y: 116, width: 130, height: 130)
+        let image = UIImage(named: "Man_with_bowler_hat_and_overcoat_generated")
+        let imageView = UIImageView(image: image)
+        imageView.frame = imageFrame
+        imageView.layer.cornerRadius = imageView.frame.size.width / 2
+        imageView.clipsToBounds = true
+        imageView.frame = imageFrame
+        imageView.layer.borderWidth = 3
+        imageView.layer.borderColor = CGColor(red: 255, green: 255, blue: 255, alpha: 1)
+        return imageView
+    }
+
+    private func getStatusLabel() -> UILabel {
+        let labelFrame = CGRect(x: 160, y: 224, width: 200, height: 14)
+        let label = UILabel(frame: labelFrame)
+        label.text = "Напишите что-нибудь"
+        return label
+    }
+    
+    private func getShowStatusButton() -> UIButton {
+        let buttonFrame = CGRect(x: 16, y: 272, width: 398, height: 50)
+        let button = UIButton(frame: buttonFrame)
+        button.layer.cornerRadius = 14
+        // если я делаю cornerRadius = 4, как указано в макете, то выглядит совсем не так, как в макете
+        button.backgroundColor = .blue
+        button.setTitle("Показать статус", for: .normal)
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return button
+    }
+    @objc func buttonPressed() {
+        print(getStatusLabel().text)
+    }
+    
+    override init(frame: CGRect) {
+        super.init (frame: frame)
+        addSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubviews() {
+        addSubview(getLabel())
+        addSubview(getAvatar())
+        addSubview(getStatusLabel())
+        addSubview(getShowStatusButton())
+    }
+
+
+}
---
+++ Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Dmitry Gorbunow on 10/7/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    var profileHeaderView = ProfileHeaderView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        view.addSubview(profileHeaderView)
+        self.title = "Профиль"
+//        self.navigationController?.navigationBar.backgroundColor = .white
+    }
+    
+    
+    override func viewWillLayoutSubviews() {
+        profileHeaderView.frame = view.frame
+        
+    }
+}
