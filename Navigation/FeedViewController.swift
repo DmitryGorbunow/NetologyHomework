143f324a73ae1bf776229715425a07a0e33a3447
DmitryGorbunow <84672242+DmitryGorbunow@users.noreply.github.com>
2022-11-30 18:02:03 +0500

    add new button in ProfileViewController and add post buttons in FeedViewController

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -11,18 +11,64 @@ class FeedViewController: UIViewController {
     
     let post = Post(title: "Это пост")
     
+    private lazy var vStack: UIStackView = {
+        
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 10
+        
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    private lazy var openPostButton1: UIButton = {
+        let setStatusButton = UIButton()
+        setStatusButton.setTitle("Открыть пост", for: .normal)
+        setStatusButton.backgroundColor = .systemBlue
+        setStatusButton.titleLabel?.textColor = .white
+        setStatusButton.layer.cornerRadius = 14
+        setStatusButton.layer.shadowColor = UIColor.black.cgColor
+        setStatusButton.layer.shadowOffset = CGSize(width: 4, height: 4)
+        setStatusButton.layer.shadowRadius = 4
+        setStatusButton.layer.shadowOpacity = 0.7
+        setStatusButton.addTarget(self, action: #selector(openPostButtonPressed), for: .touchUpInside)
+        setStatusButton.translatesAutoresizingMaskIntoConstraints = false
+        return setStatusButton
+    }()
+    
+    private lazy var openPostButton2: UIButton = {
+        let setStatusButton = UIButton()
+        setStatusButton.setTitle("Открыть пост", for: .normal)
+        setStatusButton.backgroundColor = .systemBlue
+        setStatusButton.titleLabel?.textColor = .white
+        setStatusButton.layer.cornerRadius = 14
+        setStatusButton.layer.shadowColor = UIColor.black.cgColor
+        setStatusButton.layer.shadowOffset = CGSize(width: 4, height: 4)
+        setStatusButton.layer.shadowRadius = 4
+        setStatusButton.layer.shadowOpacity = 0.7
+        setStatusButton.addTarget(self, action: #selector(openPostButtonPressed), for: .touchUpInside)
+        setStatusButton.translatesAutoresizingMaskIntoConstraints = false
+        return setStatusButton
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        let uiButton = UIButton()
-        self.view.addSubview(uiButton)
-        uiButton.setTitle("Пост", for: .normal)
-        uiButton.setTitleColor(.black, for: .normal)
-        uiButton.translatesAutoresizingMaskIntoConstraints = false
-        uiButton.addAction(UIAction(handler: {_ in
-            let vc = PostViewController()
-            self.navigationController?.pushViewController(vc, animated: true)
-        }), for: .touchUpInside)
-        NSLayoutConstraint.activate([uiButton.centerYAnchor.constraint(equalTo: view.centerYAnchor), uiButton.leadingAnchor.constraint(equalTo: view.leadingAnchor), uiButton.trailingAnchor.constraint(equalTo: view.trailingAnchor), uiButton.heightAnchor.constraint(equalToConstant: 30)])
+        view.backgroundColor = .systemBackground
+        view.addSubview(vStack)
+        vStack.addArrangedSubview(openPostButton1)
+        vStack.addArrangedSubview(openPostButton2)
+        NSLayoutConstraint.activate([
+            vStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            vStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            vStack.heightAnchor.constraint(equalToConstant: view.frame.height / 8),
+            vStack.widthAnchor.constraint(equalToConstant: view.frame.width / 2)
+            
+        ])
+    }
+    
+    @objc func openPostButtonPressed() {
+        let vc = PostViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 }
